2b0bf4b92ab9f6f6c5f3d52851d372e665f7994e
Daniel Lucas <daniellucas_santos@hotmail.com>
2021-10-15 15:46:23 -0300

    Update teste 01.py

--- teste 01.py
+++ teste 01.py
@@ -1,4 +1,4 @@
 nome = input ('Qual é o seu nome?')
 idade = input ('Quantos anos você tem?')
 peso =  input ('Quanto você pesa?')
-print (f'Nome: {nome}\tdade: {idade}\tPeso: {peso}')
+print (f'Nome: {nome}\tIdade: {idade}\tPeso: {peso}')

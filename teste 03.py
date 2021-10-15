8182142649086addffec967a6773c85cdf10c563
Daniel-LucasS <daniellucas_santos@hotmail.com>
2021-10-15 15:40:46 -0300

    primeiro commit

---
+++ teste 03.py
@@ -0,0 +1,4 @@
+dia = input (' Qual o dia em que você nasceu?')
+mês = input ('Qual o mês do seu nascimento?')
+ano = input ('Qual o ano em que você nasceu?')
+print ('Você nasceu no dia' ,dia, ',de' ,mês, 'de' ,ano, '.')
---
+++ teste 04.py
@@ -0,0 +1,5 @@
+numero_1 =  input ( 'Qual o primeiro numero?')
+numero_2 = input ( 'Qual o segundo numero?')
+print ('A soma dos numeros é:', numero_1 + numero_2,)
+
+
